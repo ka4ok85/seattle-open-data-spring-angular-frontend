2b529de46983fba53b228eae87c8ee6cebf3c2ba
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-07-07 19:38:24 -0400

    added routing for calls by type component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,13 @@
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from "./home.component";
+import {ByTypeComponent} from "./by-type.component";
 import {AboutComponent} from "./about.component";
 import {ProductDetailComponentParam} from "./product-param.component";
 import { EnvironmentSpecificResolver } from './core/services/environment-specific-resolver.service';
 
 const routes: Routes = [
   {path: '',        component: HomeComponent, resolve: { envSpecific: EnvironmentSpecificResolver }},
+  {path: 'by-type', component: ByTypeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'product/:id', component: ProductDetailComponentParam}
 ];
