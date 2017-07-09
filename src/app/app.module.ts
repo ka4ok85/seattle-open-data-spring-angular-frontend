5ca1b502bf492258a767d8c4692e8aca73db4491
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-07-08 20:54:16 -0400

    updated required dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from "@angular/router";
 
 import { AppComponent } from './app.component';
+import {HashLocationStrategy, LocationStrategy} from "@angular/common";
+import {PathLocationStrategy} from "@angular/common";
+import { HttpModule } from '@angular/http';
+
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+import 'chart.js/src/chart.js';
+
+import {AboutComponent} from "./about.component";
+import {HomeComponent} from "./home.component";
+import {ByTypeComponent} from "./by-type.component";
+import {routing} from "./app.routing";
+import {ProductDetailComponentParam} from "./product-param.component";
+
+import {EnvironmentSpecificService} from "./core/services/environment-specific.service";
+import {EnvironmentSpecificResolver} from "./core/services/environment-specific-resolver.service";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HomeComponent, ByTypeComponent, AboutComponent, ProductDetailComponentParam
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule, routing, ChartsModule, HttpModule
   ],
-  providers: [],
+  providers:[{provide: LocationStrategy, useClass: PathLocationStrategy}, EnvironmentSpecificService, EnvironmentSpecificResolver],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
