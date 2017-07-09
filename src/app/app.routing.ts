6c06c9aa2382033523b8c8c837a27f0a246a7506
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-07-09 15:24:38 -0400

    added route for ByTypeSpecific

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,13 +1,15 @@
-import { Routes, RouterModule } from '@angular/router';
+import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from "./home.component";
 import {ByTypeComponent} from "./by-type.component";
+import {ByTypeSpecificComponent} from "./by-type-specific.component";
 import {AboutComponent} from "./about.component";
 import {ProductDetailComponentParam} from "./product-param.component";
-import { EnvironmentSpecificResolver } from './core/services/environment-specific-resolver.service';
+import {EnvironmentSpecificResolver} from './core/services/environment-specific-resolver.service';
 
 const routes: Routes = [
   {path: '',        component: HomeComponent, resolve: { envSpecific: EnvironmentSpecificResolver }},
-  {path: 'by-type', component: ByTypeComponent},
+  {path: 'by-type', component: ByTypeComponent, resolve: { envSpecific: EnvironmentSpecificResolver }},
+  {path: 'by-type/:type', component: ByTypeSpecificComponent, resolve: { envSpecific: EnvironmentSpecificResolver }},
   {path: 'about', component: AboutComponent},
   {path: 'product/:id', component: ProductDetailComponentParam}
 ];
