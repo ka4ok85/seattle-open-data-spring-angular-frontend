b6635321bff70340eea4c821476294ba1d0ecd67
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-07-12 13:04:20 -0400

    added by-zip route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,7 @@ import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from "./home.component";
 import {ByTypeComponent} from "./by-type.component";
 import {ByTypeSpecificComponent} from "./by-type-specific.component";
+import {ByZipComponent} from "./by-zip.component";
 import {AboutComponent} from "./about.component";
 import {ProductDetailComponentParam} from "./product-param.component";
 import {EnvironmentSpecificResolver} from './core/services/environment-specific-resolver.service';
@@ -9,9 +10,9 @@ import {EnvironmentSpecificResolver} from './core/services/environment-specific-
 const routes: Routes = [
   {path: '',        component: HomeComponent, resolve: { envSpecific: EnvironmentSpecificResolver }},
   {path: 'by-type', component: ByTypeComponent, resolve: { envSpecific: EnvironmentSpecificResolver }},
+  {path: 'by-zip', component: ByZipComponent, resolve: { envSpecific: EnvironmentSpecificResolver }},
   {path: 'by-type/:type', component: ByTypeSpecificComponent, resolve: { envSpecific: EnvironmentSpecificResolver }},
   {path: 'about', component: AboutComponent},
-  {path: 'product/:id', component: ProductDetailComponentParam}
 ];
 
 export const routing = RouterModule.forRoot(routes);
