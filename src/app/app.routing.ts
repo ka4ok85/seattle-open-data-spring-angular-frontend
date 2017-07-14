fb3c8c3f6fb8e20b46014ce4e7fc5291ba6c6126
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-07-14 12:43:19 -0400

    added zip-specific route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,6 +3,7 @@ import {HomeComponent} from "./home.component";
 import {ByTypeComponent} from "./by-type.component";
 import {ByTypeSpecificComponent} from "./by-type-specific.component";
 import {ByZipComponent} from "./by-zip.component";
+import {ByZipSpecificComponent} from "./by-zip-specific.component";
 import {AboutComponent} from "./about.component";
 import {ProductDetailComponentParam} from "./product-param.component";
 import {EnvironmentSpecificResolver} from './core/services/environment-specific-resolver.service';
@@ -10,8 +11,9 @@ import {EnvironmentSpecificResolver} from './core/services/environment-specific-
 const routes: Routes = [
   {path: '',        component: HomeComponent, resolve: { envSpecific: EnvironmentSpecificResolver }},
   {path: 'by-type', component: ByTypeComponent, resolve: { envSpecific: EnvironmentSpecificResolver }},
-  {path: 'by-zip', component: ByZipComponent, resolve: { envSpecific: EnvironmentSpecificResolver }},
   {path: 'by-type/:type', component: ByTypeSpecificComponent, resolve: { envSpecific: EnvironmentSpecificResolver }},
+  {path: 'by-zip', component: ByZipComponent, resolve: { envSpecific: EnvironmentSpecificResolver }},
+  {path: 'by-zip/:zip', component: ByZipSpecificComponent, resolve: { envSpecific: EnvironmentSpecificResolver }},
   {path: 'about', component: AboutComponent},
 ];
 
