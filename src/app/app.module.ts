a3493046bf8124aedf307fb63430d4b2967efd37
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-07-16 10:16:03 -0400

    added date range utils service to dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {RouterModule} from "@angular/router";
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
-import {HashLocationStrategy, LocationStrategy} from "@angular/common";
-import {PathLocationStrategy} from "@angular/common";
+import { HashLocationStrategy, LocationStrategy } from "@angular/common";
+import { PathLocationStrategy } from "@angular/common";
 import { HttpModule } from '@angular/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 import 'chart.js/src/chart.js';
 
-import {AboutComponent} from "./about.component";
-import {HomeComponent} from "./home.component";
-import {ByTypeComponent} from "./by-type.component";
-import {ByTypeSpecificComponent} from "./by-type-specific.component";
-import {ByZipComponent} from "./by-zip.component";
-import {ByZipSpecificComponent} from "./by-zip-specific.component";
-import {routing} from "./app.routing";
-import {ProductDetailComponentParam} from "./product-param.component";
+import { AboutComponent } from "./about.component";
+import { HomeComponent } from "./home.component";
+import { ByTypeComponent } from "./by-type.component";
+import { ByTypeSpecificComponent } from "./by-type-specific.component";
+import { ByZipComponent } from "./by-zip.component";
+import { ByZipSpecificComponent } from "./by-zip-specific.component";
+import { routing } from "./app.routing";
+import { ProductDetailComponentParam } from "./product-param.component";
 
-import {EnvironmentSpecificService} from "./core/services/environment-specific.service";
-import {EnvironmentSpecificResolver} from "./core/services/environment-specific-resolver.service";
+import { EnvironmentSpecificService } from "./core/services/environment-specific.service";
+import { EnvironmentSpecificResolver } from "./core/services/environment-specific-resolver.service";
+import { DateRangeUtils } from "./core/services/date-range-utils.service";
 
 import { MyDatePickerModule } from 'mydatepicker';
 
@@ -32,7 +33,7 @@ import { MyDatePickerModule } from 'mydatepicker';
   imports: [
     BrowserModule, RouterModule, routing, ChartsModule, HttpModule, MyDatePickerModule, FormsModule, ReactiveFormsModule
   ],
-  providers:[{provide: LocationStrategy, useClass: PathLocationStrategy}, EnvironmentSpecificService, EnvironmentSpecificResolver],
+  providers: [{ provide: LocationStrategy, useClass: PathLocationStrategy }, EnvironmentSpecificService, EnvironmentSpecificResolver, DateRangeUtils],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
