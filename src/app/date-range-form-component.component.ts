f5e9ed4c732b1baea17352ac8b24915abf91a1f7
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-07-19 15:01:47 -0400

    added date range sub-form component

---
+++ src/app/date-range-form-component.component.ts
@@ -0,0 +1,93 @@
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from "rxjs/Observable";
+
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { EventEmitter, Input, Output } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { EnvSpecific } from 'app/core/models/env-specific';
+import { DateRangeUtils } from "./core/services/date-range-utils.service";
+import { FormDateRange } from "./core/models/form-date-range";
+
+import { BaseChartDirective } from 'ng2-charts/ng2-charts';
+
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { IMyDpOptions, IMyOptions, IMyInputFieldChanged, IMyDate } from 'mydatepicker';
+
+@Component({
+  selector: 'dateRangeForm',
+  templateUrl: './templates/date-range-form.html',
+  styles: ['']
+})
+
+export class DateRangeFormComponent {
+
+  @Output() onDateRangeFormSubmit = new EventEmitter<FormDateRange>();
+
+  public myDatePickerOptions: IMyDpOptions = {
+    dateFormat: 'mm/dd/yyyy',
+    width: '150px',
+    editableDateField: false,
+  };
+
+  public rangeFormModel: FormGroup;
+  public rangeForm: FormGroup;
+
+  constructor(private http: Http, private route: ActivatedRoute, formBuilder: FormBuilder, private dateRangeUtils: DateRangeUtils) {
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    let day = date.getDate();
+
+    // sub-form
+    this.rangeForm = formBuilder.group(
+      {
+        startdate: [{ date: { year: year, month: month, day: day } }, Validators.required],
+        enddate: [{ date: { year: year, month: month + 1, day: day } }, Validators.required]
+      },
+      {
+        validator: this.startEndDatesRangeValidator.bind(this)
+      }
+    );
+
+    // main form
+    this.rangeFormModel = formBuilder.group({
+      'rangeForm': this.rangeForm
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  /* FORMS */
+  public onDatepickerFormSubmit(): void {
+    let startDateObject = this.rangeForm.controls['startdate'].value.date;
+    let startDateFormatted: string = this.dateRangeUtils.dateToIntlFormattedString(startDateObject);
+
+    let endDateObject = this.rangeForm.controls['enddate'].value.date;
+    let endDateFormatted: string = this.dateRangeUtils.dateToIntlFormattedString(endDateObject);
+
+    let formDateRange: FormDateRange = new FormDateRange();
+    formDateRange.startDate = startDateFormatted;
+    formDateRange.endDate = endDateFormatted;
+
+    this.onDateRangeFormSubmit.emit(formDateRange);
+  }
+
+  private startEndDatesRangeValidator(group: FormGroup) {
+    if (!group.controls.startdate || !group.controls.startdate.value || !group.controls.enddate || !group.controls.enddate.value) return;
+
+    let startDate = group.controls.startdate.value.date;
+    let endDate = group.controls.enddate.value.date;
+
+    let startDateNumber: number = startDate.year * 10000 + startDate.month * 100 + startDate.day;
+    let endDateNumber: number = endDate.year * 10000 + endDate.month * 100 + endDate.day;
+
+    if (startDateNumber > endDateNumber) {
+      return {
+        badRange: true
+      };
+    }
+  }
+
+}
