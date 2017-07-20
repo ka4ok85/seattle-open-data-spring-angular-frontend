857b47fcf40a3f837edcc4f0b8cc9ad0e2ff753b
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-07-20 18:21:53 -0400

    added DateRange Sub Form support for by-type-specific component

--- src/app/by-zip-specific.component.ts
+++ src/app/by-zip-specific.component.ts
@@ -5,8 +5,8 @@ import { Observable } from "rxjs/Observable";
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EnvSpecific } from 'app/core/models/env-specific';
-
-import { BaseChartDirective } from 'ng2-charts/ng2-charts';
+import { FormDateRange } from "./core/models/form-date-range";
+import { DateRangeUtils } from "./core/services/date-range-utils.service";
 
 @Component({
     selector: 'by-zip',
@@ -27,6 +27,125 @@ export class ByZipSpecificComponent {
     public lineChartLabels: Array<any> = [];
     public pieChartLabels: String[] = [];
     public pieChartData: Array<any> = [{ data: [] }];
+
+    constructor(private http: Http, private route: ActivatedRoute, private dateRangeUtils: DateRangeUtils) {
+        this.zip = route.snapshot.paramMap.get('zip');
+        console.log(this.zip);
+    }
+
+    ngOnInit() {
+        this.route.data
+            .subscribe((data: { envSpecific: EnvSpecific }) => {
+                this.apiURL = data.envSpecific.apiURL;
+            });
+
+        this.getData(30);
+    }
+
+    private getDataInternal(startDate: string, endDate: string) {
+        this.theDataSource = this.http.get(this.apiURL + 'calls/count/' + startDate + '/' + endDate + "?zip=" + this.zip).map(res => res.json());
+        this.thePieDataSource = this.http.get(this.apiURL + 'calls/count/per-type/' + startDate + '/' + endDate + "?zip=" + this.zip).map(res => res.json());
+        //this.days = days;
+
+        // Get the data from the REST server
+        this.theDataSource.subscribe(
+            data => {
+                let dataLabels: String[] = [];
+                let dataCounts: String[] = [];
+                for (let i = 0; i < data.length; i++) {
+                    dataLabels.push(data[i][1]);
+                    dataCounts.push(data[i][0]);
+                }
+
+                this.buildLineChart(dataLabels, dataCounts);
+            },
+            err => console.log("Can't get Counts. Error code: %s, URL: %s ", err.status, err.url),
+            () => console.log('Counts are retrieved')
+        );
+
+        this.thePieDataSource.subscribe(
+            data => {
+                let dataLabels: String[] = [];
+                let dataCounts: String[] = [];
+                for (let i = 0; i < data.length; i++) {
+                    dataLabels.push(data[i][1]);
+                    dataCounts.push(data[i][0]);
+                }
+
+                this.buildPieChart(dataLabels, dataCounts);
+            },
+            err => console.log("Can't get Radar Chart Counts. Error code: %s, URL: %s ", err.status, err.url),
+            () => console.log('Radar Chart Counts are retrieved')
+        );
+
+    }
+
+    public getData(days: number) {
+        let range = this.dateRangeUtils.getIntlFormattedRangeForPastDays(days);
+        this.getDataInternal(range['start'], range['end']);
+    }
+
+        /* CHARTS */
+    private buildLineChart(labels: String[], dataCounts: String[]) {
+        this.lineChartData = [{ data: dataCounts, label: this.zip + ' 911 Calls #' }];
+        let labelsCount = this.lineChartLabels.length;
+        for (var index = 0; index < labelsCount; index++) {
+            this.lineChartLabels.pop();
+        }
+
+        for (let label of labels) {
+            this.lineChartLabels.push(label);
+        }
+    }
+
+    private buildPieChart(labels: String[], dataCounts: String[]) {
+        this.pieChartData = [{ data: dataCounts, label: this.zip + ' 911 Calls #' }];
+        let labelsCount = this.pieChartLabels.length;
+        for (var index = 0; index < labelsCount; index++) {
+            this.pieChartLabels.pop();
+        }
+
+        for (let label of labels) {
+            this.pieChartLabels.push(label);
+        }
+
+    }
+    public lineChartOptions: any = {
+        responsive: true,
+        scales: {
+            xAxes: [{
+                type: 'time',
+                time: {
+                    displayFormats: {
+                        day: 'YYYY-MM-DD'
+                    }
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    };
+    public lineChartColors: Array<any> = [
+        {
+            backgroundColor: 'rgba(0, 131, 154,0.2)',
+            borderColor: 'rgba(0, 131, 154,1)',
+            pointBackgroundColor: 'rgba(0, 131, 154,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(0, 131, 154,0.8)'
+        }
+    ];
+    public lineChartLegend: boolean = true;
+    public lineChartType: string = 'line';
+    public chartClicked(e: any): void {
+    }
+    public chartHovered(e: any): void {
+    }
+
+    public pieChartType: string = 'pie';
     public pieChartColors: {}[] = [{
         backgroundColor: [
       '#5d8aa8',
@@ -120,137 +239,10 @@ export class ByZipSpecificComponent {
         ]
     }]
 
-    constructor(private http: Http, private route: ActivatedRoute) {
-        this.zip = route.snapshot.paramMap.get('zip');
-        console.log(this.zip);
-    }
-
-    ngOnInit() {
-        this.route.data
-            .subscribe((data: { envSpecific: EnvSpecific }) => {
-                this.apiURL = data.envSpecific.apiURL;
-            });
-
-        this.getData(90);
-    }
-
-    public getData(days: Number) {
-
-        console.log(this.apiURL);
-        this.theDataSource = this.http.get(this.apiURL + 'calls/count/' + days + "?zip=" + this.zip).map(res => res.json());
-        this.thePieDataSource = this.http.get(this.apiURL + 'calls/count/per-type/' + days + "?zip=" + this.zip).map(res => res.json());
-        this.days = days;
-
-        // Get the data from the REST server
-        this.theDataSource.subscribe(
-            data => {
-                let dataLabels: String[] = [];
-                let dataCounts: String[] = [];
-                for (let i = 0; i < data.length; i++) {
-                    dataLabels.push(data[i][1]);
-                    dataCounts.push(data[i][0]);
-                }
-
-                this.buildLineChart(dataLabels, dataCounts);
-            },
-            err => console.log("Can't get Counts. Error code: %s, URL: %s ", err.status, err.url),
-            () => console.log('Counts are retrieved')
-        );
-
-        this.thePieDataSource.subscribe(
-            data => {
-                let dataLabels: String[] = [];
-                let dataCounts: String[] = [];
-                for (let i = 0; i < data.length; i++) {
-                    dataLabels.push(data[i][1]);
-                    dataCounts.push(data[i][0]);
-                }
-
-                this.buildPieChart(dataLabels, dataCounts);
-            },
-            err => console.log("Can't get Radar Chart Counts. Error code: %s, URL: %s ", err.status, err.url),
-            () => console.log('Radar Chart Counts are retrieved')
-        );
-
-    }
-
-    private buildLineChart(labels: String[], dataCounts: String[]) {
-        this.lineChartData = [{ data: dataCounts, label: this.zip + ' 911 Calls #' }];
-        let labelsCount = this.lineChartLabels.length;
-        for (var index = 0; index < labelsCount; index++) {
-            this.lineChartLabels.pop();
-        }
-
-        for (let label of labels) {
-            this.lineChartLabels.push(label);
-        }
-    }
-
-    private buildPieChart(labels: String[], dataCounts: String[]) {
-        this.pieChartData = [{ data: dataCounts, label: this.zip + ' 911 Calls #' }];
-        let labelsCount = this.pieChartLabels.length;
-        for (var index = 0; index < labelsCount; index++) {
-            this.pieChartLabels.pop();
-        }
-
-        for (let label of labels) {
-            this.pieChartLabels.push(label);
-        }
-
-    }
-    public lineChartOptions: any = {
-        responsive: true,
-        scales: {
-            xAxes: [{
-                type: 'time',
-                time: {
-                    displayFormats: {
-                        day: 'YYYY-MM-DD'
-                    }
-                }
-            }],
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    };
-
-    public lineChartColors: Array<any> = [
-        { // grey
-            backgroundColor: 'rgba(0, 131, 154,0.2)',
-            borderColor: 'rgba(0, 131, 154,1)',
-            pointBackgroundColor: 'rgba(0, 131, 154,1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(0, 131, 154,0.8)'
-        }
-    ];
-    public lineChartLegend: boolean = true;
-    public lineChartType: string = 'line';
-
-    // events
-    public chartClicked(e: any): void {
-        //console.log(e);
-    }
-
-    public chartHovered(e: any): void {
-        //console.log(e);
-    }
-
-    public pieChartType: string = 'pie';
-
-    // events
     public chartPieClicked(e: any): void {
-        console.log(e);
     }
-
     public chartPieHovered(e: any): void {
-        console.log(e);
     }
-
-
     public pieChartOptions: any = {
         responsive: true,
         legend: {
@@ -258,4 +250,9 @@ export class ByZipSpecificComponent {
             position: "right"
         }
     };
+
+    /* FORMS */
+    public onDateRangeFormSubmit(formDateRange: FormDateRange): void {
+        this.getDataInternal(formDateRange.startDate, formDateRange.endDate);
+    }
 }
