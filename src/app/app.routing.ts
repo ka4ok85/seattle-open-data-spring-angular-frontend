b5f771621c4ecdb85c05d90781c99c51ac5e9c86
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-08-10 20:00:40 -0400

    added hourly route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,19 +1,30 @@
-import {Routes, RouterModule} from '@angular/router';
-import {HomeComponent} from "./home.component";
-import {ByTypeComponent} from "./by-type.component";
-import {ByTypeSpecificComponent} from "./by-type-specific.component";
-import {ByZipComponent} from "./by-zip.component";
-import {ByZipSpecificComponent} from "./by-zip-specific.component";
-import {AboutComponent} from "./about.component";
-import {EnvironmentSpecificResolver} from './core/services/environment-specific-resolver.service';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from "./home.component";
+import { ByTypeComponent } from "./by-type.component";
+import { ByTypeSpecificComponent } from "./by-type-specific.component";
+import { ByZipComponent } from "./by-zip.component";
+import { ByZipSpecificComponent } from "./by-zip-specific.component";
+import { HourlyComponent } from "./hourly.component";
+import { AboutComponent } from "./about.component";
+import { EnvironmentSpecificResolver } from './core/services/environment-specific-resolver.service';
 
 const routes: Routes = [
-  {path: '',        component: HomeComponent, resolve: { envSpecific: EnvironmentSpecificResolver }},
-  {path: 'by-type', component: ByTypeComponent, resolve: { envSpecific: EnvironmentSpecificResolver }},
-  {path: 'by-type/:type', component: ByTypeSpecificComponent, resolve: { envSpecific: EnvironmentSpecificResolver }},
-  {path: 'by-zip', component: ByZipComponent, resolve: { envSpecific: EnvironmentSpecificResolver }},
-  {path: 'by-zip/:zip', component: ByZipSpecificComponent, resolve: { envSpecific: EnvironmentSpecificResolver }},
-  {path: 'about', component: AboutComponent},
+  { path: '', component: HomeComponent, resolve: { envSpecific: EnvironmentSpecificResolver } },
+  { path: 'by-type', component: ByTypeComponent, resolve: { envSpecific: EnvironmentSpecificResolver } },
+  { path: 'by-type/:type', component: ByTypeSpecificComponent, resolve: { envSpecific: EnvironmentSpecificResolver } },
+  { path: 'by-zip', component: ByZipComponent, resolve: { envSpecific: EnvironmentSpecificResolver } },
+  { path: 'by-zip/:zip', component: ByZipSpecificComponent, resolve: { envSpecific: EnvironmentSpecificResolver } },
+  {
+    path: 'hourly', component: HourlyComponent, resolve: { envSpecific: EnvironmentSpecificResolver },
+    children: [
+      { path: 'by-type/:type', component: ByTypeComponent, resolve: { envSpecific: EnvironmentSpecificResolver } },
+      { path: 'by-zip/:zip', component: ByTypeComponent, resolve: { envSpecific: EnvironmentSpecificResolver } },
+      { path: '', component: ByTypeComponent, resolve: { envSpecific: EnvironmentSpecificResolver } }
+    ]
+  },
+
+  { path: 'about', component: AboutComponent },
 ];
+
 
 export const routing = RouterModule.forRoot(routes);
