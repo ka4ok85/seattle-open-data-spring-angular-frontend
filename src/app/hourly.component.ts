1942696627f252f47f8dcb06881828daa37f96d1
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-08-13 17:49:54 -0400

    removed unused functionality from Hourly component

--- src/app/hourly.component.ts
+++ src/app/hourly.component.ts
@@ -1,12 +1,6 @@
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
-import { Observable } from "rxjs/Observable";
-
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { EnvSpecific } from 'app/core/models/env-specific';
+import { Component } from '@angular/core';
 import { FormDateRange } from "./core/models/form-date-range";
-import { DateRangeUtils } from "./core/services/date-range-utils.service";
+import { DateUpdatesService } from './core/services/date-updates.service';
 
 @Component({
     selector: '',
@@ -15,106 +9,14 @@ import { DateRangeUtils } from "./core/services/date-range-utils.service";
 })
 
 export class HourlyComponent {
-    theDataSource: Observable<string>;
-    apiURL: string;
-    days: Number;
-    busy: Promise<any>;
 
-    public rawData = [];
-    public barChartData: Array<any> = [{ data: [] }];
-    public barChartLabels: string[] = [];
-
-    constructor(protected http: Http, protected route: ActivatedRoute, protected dateRangeUtils: DateRangeUtils) {
-
-    }
-
-    ngOnInit() {
-        this.route.data
-            .subscribe((data: { envSpecific: EnvSpecific }) => {
-                this.apiURL = data.envSpecific.apiURL;
-            });
-
-        this.getData(30);
-    }
-
-    public getData(days: number) {
-        let range = this.dateRangeUtils.getIntlFormattedRangeForPastDays(days);
-        this.getDataInternal(range['start'], range['end']);
-    }
-
-    private getDataInternal(startDate: string, endDate: string) {
-        this.theDataSource = this.http.get(this.apiURL + 'calls/count/' + startDate + '/' + endDate + '/?hourly').map(res => res.json());
-        this.busy = this.theDataSource.toPromise();
-        //this.days = days;
-        this.rawData = [];
-
-        // Get the data from the REST server
-        this.theDataSource.subscribe(
-            data => {
-                let dataLabels: string[] = [];
-                let dataCounts: string[] = [];
-
-                for (let i = 0; i < data.length; i++) {
-                    dataLabels.push(data[i][1]);
-                    dataCounts.push(data[i][0]);
-                }
-
-                this.buildBarChart(dataLabels, dataCounts);
-            },
-            err => console.log("Can't get Counts. Error code: %s, URL: %s ", err.status, err.url),
-            () => console.log('Counts are retrieved')
-        );
-
-    }
-
-    /* CHARTS */
-    private buildBarChart(labels: string[], dataCounts: string[]) {
-        this.barChartData = [{ data: dataCounts, label: 'Hourly Breakdown' }];
-        let labelsCount = this.barChartLabels.length;
-        for (var index = 0; index < labelsCount; index++) {
-            this.barChartLabels.pop();
-        }
-
-        for (let label of labels) {
-            this.barChartLabels.push(label);
-        }
-    }
-
-    public barChartType: string = 'bar';
-    public barChartLegend: boolean = true;
-    public barChartOptions: any = {
-        scaleShowVerticalLines: false,
-        responsive: true,
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    };
-    public barChartColors: Array<any> = [
-        {
-            backgroundColor: 'rgba(0, 131, 154,0.2)',
-            borderColor: 'rgba(0, 131, 154,1)',
-            pointBackgroundColor: 'rgba(0, 131, 154,1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(0, 131, 154,0.8)'
-        }
-    ];
-    public chartClicked(e: any): void {
-    }
-    public chartHovered(e: any): void {
-    }
-
-    /* FORMS */
-    public onDateRangeFormSubmit(formDateRange: FormDateRange): void {
-        this.getDataInternal(formDateRange.startDate, formDateRange.endDate);
-    }
+    constructor(private dateUpdatesService: DateUpdatesService) { }
 
     public onDateRangeQuickButtonClick(days: number): void {
-        let range = this.dateRangeUtils.getIntlFormattedRangeForPastDays(days);
-        this.getDataInternal(range['start'], range['end']);
+        this.dateUpdatesService.passDays(days);
+    }
+
+    public onDateRangeFormSubmit(formDateRange: FormDateRange): void {
+        this.dateUpdatesService.passDateRange(formDateRange);
     }
 }
