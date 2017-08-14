87944579fa3c0e3fd93848853c49411e84fd18b0
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-08-14 19:57:55 -0400

    added by zip hourly child component

---
+++ src/app/hourly-by-zip.component.ts
@@ -0,0 +1,55 @@
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from "rxjs/Observable";
+
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { EnvSpecific } from 'app/core/models/env-specific';
+import { FormDateRange } from "./core/models/form-date-range";
+import { DateRangeUtils } from "./core/services/date-range-utils.service";
+import { HourlyAbstractComponent } from "./hourly-abstract.component";
+import { EventEmitter, Input, Output } from '@angular/core';
+import { DateUpdatesService } from './core/services/date-updates.service';
+
+@Component({
+    selector: '',
+    templateUrl: './templates/hourly/by-zip.html',
+    styles: ['']
+})
+
+export class HourlyByZipComponent extends HourlyAbstractComponent {
+
+    private zip: string;
+
+    constructor(protected http: Http, protected route: ActivatedRoute, protected dateRangeUtils: DateRangeUtils, private dateUpdatesService: DateUpdatesService) {
+        super(http, route, dateRangeUtils);
+        this.zip = route.snapshot.paramMap.get('zip');
+        this.daysSubscription = dateUpdatesService.daysPassed$.subscribe(
+            mission => {
+                let dateRange: FormDateRange = this.dateRangeUtils.getFormDateRangeForPastDays(mission);
+                this.setHourlyCountsApiURL(dateRange);
+                this.getData(dateRange);
+            });
+
+        this.dateRangeSubscription = dateUpdatesService.dateRangePassed$.subscribe(
+            dateRange => {
+                this.setHourlyCountsApiURL(dateRange);
+                this.getData(dateRange);
+            });
+    }
+
+    ngOnInit() {
+        this.route.data
+            .subscribe((data: { envSpecific: EnvSpecific }) => {
+                this.apiURL = data.envSpecific.apiURL;
+            });
+
+        let dateRange: FormDateRange = this.dateRangeUtils.getFormDateRangeForPastDays(30);
+        this.setHourlyCountsApiURL(dateRange);
+        this.getData(dateRange);
+    }
+
+    public setHourlyCountsApiURL(dateRange: FormDateRange) {
+        this.hourlyCountsApiURL = this.apiURL + 'calls/count/' + dateRange.startDate + '/' + dateRange.endDate + '/?zip=' + this.zip + '&hourly';
+    }
+}
