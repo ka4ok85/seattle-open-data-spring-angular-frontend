95d7c3dfbd9397ab33bcd42c15162d63c5344fa1
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-08-17 18:54:26 -0400

    added Angulartics2GoogleAnalytics to the App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { EnvironmentSpecificService } from './core/services/environment-specific.service';
 import { EnvSpecific } from './core/models/env-specific';
 import { EnvironmentSpecificResolver } from './core/services/environment-specific-resolver.service';
+import { Angulartics2GoogleAnalytics } from 'angulartics2';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,7 @@ export class AppComponent {
   title = 'app';
   apiURL: string;
 
-  constructor(envSpecificSvc: EnvironmentSpecificService) {
+  constructor(envSpecificSvc: EnvironmentSpecificService, angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics) {
     envSpecificSvc.subscribe(this, this.setApiURL);
   }
 
