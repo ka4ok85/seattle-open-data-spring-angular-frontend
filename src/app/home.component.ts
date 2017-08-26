c335172eea87bb2ba994130acbef5ec4b3cf7f3d
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-08-26 17:08:31 -0400

    set title for home page

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,3 +1,4 @@
+import { Title } from '@angular/platform-browser';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from "rxjs/Observable";
@@ -11,261 +12,262 @@ import { DateRangeUtils } from "./core/services/date-range-utils.service";
 import { IMyDpOptions, IMyOptions, IMyInputFieldChanged, IMyDate } from 'mydatepicker';
 
 @Component({
-  selector: '',
-  templateUrl: './templates/home.html',
-  styles: ['']
+    selector: '',
+    templateUrl: './templates/home.html',
+    styles: ['']
 })
 
 export class HomeComponent {
-  theDataSource: Observable<string>;
-  thePieDataSource: Observable<string>;
-  apiURL: string;
-  days: Number;
-  busy: Promise<any>;
+    theDataSource: Observable<string>;
+    thePieDataSource: Observable<string>;
+    apiURL: string;
+    days: Number;
+    busy: Promise<any>;
 
-  public startDate;
-  public endDate;
-  public lineChartData: Array<any> = [{ data: [] }];
-  public lineChartLabels: Array<any> = [];
-  public pieChartLabels: String[] = [];
-  public pieChartData: String[] = [];
+    public startDate;
+    public endDate;
+    public lineChartData: Array<any> = [{ data: [] }];
+    public lineChartLabels: Array<any> = [];
+    public pieChartLabels: String[] = [];
+    public pieChartData: String[] = [];
 
-  constructor(private http: Http, private route: ActivatedRoute, private dateRangeUtils: DateRangeUtils) {
-  }
+    constructor(private http: Http, private route: ActivatedRoute, private dateRangeUtils: DateRangeUtils, private titleService: Title) {
+    }
 
-  ngOnInit() {
-    this.route.data
-      .subscribe((data: { envSpecific: EnvSpecific }) => {
-        this.apiURL = data.envSpecific.apiURL;
-      });
+    ngOnInit() {
+        this.route.data
+            .subscribe((data: { envSpecific: EnvSpecific }) => {
+                this.titleService.setTitle(data.envSpecific.title + ' | Home');
+                this.apiURL = data.envSpecific.apiURL;
+            });
 
-    this.getData(30);
-  }
+        this.getData(30);
+    }
 
-  public getData(days: number) {
-    let range = this.dateRangeUtils.getIntlFormattedRangeForPastDays(days);
-    this.getDataInternal(range['start'], range['end']);
-  }
+    public getData(days: number) {
+        let range = this.dateRangeUtils.getIntlFormattedRangeForPastDays(days);
+        this.getDataInternal(range['start'], range['end']);
+    }
 
-  private getDataInternal(startDate: string, endDate: string) {
-    this.startDate = startDate;
-    this.endDate = endDate;
-    this.theDataSource = this.http.get(this.apiURL + 'calls/count/' + startDate + '/' + endDate).map(res => res.json());
-    this.thePieDataSource = this.http.get(this.apiURL + 'calls/count/per-type/' + startDate + '/' + endDate).map(res => res.json());
-    this.busy = this.theDataSource.toPromise();
-    //this.days = days;
+    private getDataInternal(startDate: string, endDate: string) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.theDataSource = this.http.get(this.apiURL + 'calls/count/' + startDate + '/' + endDate).map(res => res.json());
+        this.thePieDataSource = this.http.get(this.apiURL + 'calls/count/per-type/' + startDate + '/' + endDate).map(res => res.json());
+        this.busy = this.theDataSource.toPromise();
+        //this.days = days;
 
-    this.theDataSource.subscribe(
-      data => {
-        let dataLabels: String[] = [];
-        let dataCounts: String[] = [];
-        for (let i = 0; i < data.length; i++) {
-          dataLabels.push(data[i][1]);
-          dataCounts.push(data[i][0]);
+        this.theDataSource.subscribe(
+            data => {
+                let dataLabels: String[] = [];
+                let dataCounts: String[] = [];
+                for (let i = 0; i < data.length; i++) {
+                    dataLabels.push(data[i][1]);
+                    dataCounts.push(data[i][0]);
+                }
+
+                this.buildLineChart(dataLabels, dataCounts);
+            },
+            err => console.log("Can't get Counts. Error code: %s, URL: %s ", err.status, err.url),
+            () => console.log('Counts are retrieved')
+        );
+
+        this.thePieDataSource.subscribe(
+            data => {
+                let dataLabels: String[] = [];
+                let dataCounts: String[] = [];
+                for (let i = 0; i < data.length; i++) {
+                    dataLabels.push(data[i][1]);
+                    dataCounts.push(data[i][0]);
+                }
+
+                this.buildPieChart(dataLabels, dataCounts);
+            },
+            err => console.log("Can't get Pie Chart Counts. Error code: %s, URL: %s ", err.status, err.url),
+        );
+    }
+
+    /* CHARTS */
+    private buildLineChart(labels: String[], dataCounts: String[]) {
+        this.lineChartData = [{ data: dataCounts, label: 'Number of 9-1-1 calls per day' }];
+        let labelsCount = this.lineChartLabels.length;
+        for (var index = 0; index < labelsCount; index++) {
+            this.lineChartLabels.pop();
         }
 
-        this.buildLineChart(dataLabels, dataCounts);
-      },
-      err => console.log("Can't get Counts. Error code: %s, URL: %s ", err.status, err.url),
-      () => console.log('Counts are retrieved')
-    );
+        for (let label of labels) {
+            this.lineChartLabels.push(label);
+        }
+    }
 
-    this.thePieDataSource.subscribe(
-      data => {
-        let dataLabels: String[] = [];
-        let dataCounts: String[] = [];
-        for (let i = 0; i < data.length; i++) {
-          dataLabels.push(data[i][1]);
-          dataCounts.push(data[i][0]);
+    private buildPieChart(labels: String[], dataCounts: String[]) {
+        this.pieChartColors.length = 0;
+        this.pieChartData = dataCounts;
+        let labelsCount = this.pieChartLabels.length;
+        for (var index = 0; index < labelsCount; index++) {
+            this.pieChartLabels.pop();
         }
 
-        this.buildPieChart(dataLabels, dataCounts);
-      },
-      err => console.log("Can't get Pie Chart Counts. Error code: %s, URL: %s ", err.status, err.url),
-    );
-  }
+        // Init colors
+        var colorObject = {
+            backgroundColor: []
+        };
 
-  /* CHARTS */
-  private buildLineChart(labels: String[], dataCounts: String[]) {
-    this.lineChartData = [{ data: dataCounts, label: 'Number of 9-1-1 calls per day' }];
-    let labelsCount = this.lineChartLabels.length;
-    for (var index = 0; index < labelsCount; index++) {
-      this.lineChartLabels.pop();
+        for (let label of labels) {
+            this.pieChartLabels.push(label);
+        }
+
+        //this.pieChartColors.push(colorObject); // controlled dynamic color set is not needed for now
     }
 
-    for (let label of labels) {
-      this.lineChartLabels.push(label);
-    }
-  }
-
-  private buildPieChart(labels: String[], dataCounts: String[]) {
-    this.pieChartColors.length = 0;
-    this.pieChartData = dataCounts;
-    let labelsCount = this.pieChartLabels.length;
-    for (var index = 0; index < labelsCount; index++) {
-      this.pieChartLabels.pop();
-    }
-
-    // Init colors
-    var colorObject = {
-      backgroundColor: []
+    public lineChartOptions: any = {
+        responsive: true,
+        scales: {
+            xAxes: [{
+                type: 'time',
+                time: {
+                    displayFormats: {
+                        day: 'YYYY-MM-DD'
+                    }
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
     };
 
-    for (let label of labels) {
-      this.pieChartLabels.push(label);
+    public lineChartType: string = 'line';
+    public chartClicked(e: any): void {
     }
-
-    //this.pieChartColors.push(colorObject); // controlled dynamic color set is not needed for now
-  }
-
-  public lineChartOptions: any = {
-    responsive: true,
-    scales: {
-      xAxes: [{
-        type: 'time',
-        time: {
-          displayFormats: {
-            day: 'YYYY-MM-DD'
-          }
+    public chartHovered(e: any): void {
+    }
+    public lineChartColors: Array<any> = [
+        {
+            backgroundColor: 'rgba(0, 131, 154,0.2)',
+            borderColor: 'rgba(0, 131, 154,1)',
+            pointBackgroundColor: 'rgba(0, 131, 154,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(0, 131, 154,0.8)'
         }
-      }],
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
+    ];
+    public lineChartLegend: boolean = true;
+
+    public pieChartType: string = 'pie';
+    public chartPieClicked(e: any): void {
+    }
+    public chartPieHovered(e: any): void {
+    }
+    public pieChartOptions: any = {
+        responsive: true,
+        legend: {
+            display: false,
+            position: "right"
         }
-      }]
+    };
+    public pieChartColors: {}[] = [{
+        backgroundColor: [
+            '#5d8aa8',
+            '#e32636',
+            '#efdecd',
+            '#e52b50',
+            '#ffbf00',
+            '#ff033e',
+            '#9966cc',
+            '#a4c639',
+            '#f2f3f4',
+            '#cd9575',
+            '#915c83',
+            '#faebd7',
+            '#008000',
+            '#8db600',
+            '#fbceb1',
+            '#00ffff',
+            '#7fffd4',
+            '#4b5320',
+            '#e9d66b',
+            '#b2beb5',
+            '#87a96b',
+            '#ff9966',
+            '#a52a2a',
+            '#fdee00',
+            '#6e7f80',
+            '#ff2052',
+            '#007fff',
+            '#f0ffff',
+            '#89cff0',
+            '#a1caf1',
+            '#f4c2c2',
+            '#21abcd',
+            '#fae7b5',
+            '#ffe135',
+            '#848482',
+            '#98777b',
+            '#bcd4e6',
+            '#9f8170',
+            '#f5f5dc',
+            '#ffe4c4',
+            '#3d2b1f',
+            '#fe6f5e',
+            '#000000',
+            '#ffebcd',
+            '#318ce7',
+            '#ace5ee',
+            '#faf0be',
+            '#0000ff',
+            '#a2a2d0',
+            '#6699cc',
+            '#0d98ba',
+            '#8a2be2',
+            '#de5d83',
+            '#79443b',
+            '#0095b6',
+            '#e3dac9',
+            '#cc0000',
+            '#006a4e',
+            '#873260',
+            '#0070ff',
+            '#b5a642',
+            '#cb4154',
+            '#1dacd6',
+            '#66ff00',
+            '#bf94e4',
+            '#c32148',
+            '#ff007f',
+            '#08e8de',
+            '#d19fe8',
+            '#f4bbff',
+            '#ff55a3',
+            '#fb607f',
+            '#004225',
+            '#cd7f32',
+            '#a52a2a',
+            '#ffc1cc',
+            '#e7feff',
+            '#f0dc82',
+            '#480607',
+            '#800020',
+            '#deb887',
+            '#cc5500',
+            '#e97451',
+            '#8a3324',
+            '#bd33a4',
+            '#702963',
+            '#007aa5',
+            '#e03c31'
+        ]
+    }];
+
+    /* FORMS */
+    public onDateRangeFormSubmit(formDateRange: FormDateRange): void {
+        this.getDataInternal(formDateRange.startDate, formDateRange.endDate);
     }
-  };
 
-  public lineChartType: string = 'line';
-  public chartClicked(e: any): void {
-  }
-  public chartHovered(e: any): void {
-  }
-  public lineChartColors: Array<any> = [
-    {
-      backgroundColor: 'rgba(0, 131, 154,0.2)',
-      borderColor: 'rgba(0, 131, 154,1)',
-      pointBackgroundColor: 'rgba(0, 131, 154,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(0, 131, 154,0.8)'
+    public onDateRangeQuickButtonClick(days: number): void {
+        let range = this.dateRangeUtils.getIntlFormattedRangeForPastDays(days);
+        this.getDataInternal(range['start'], range['end']);
     }
-  ];
-  public lineChartLegend: boolean = true;
-
-  public pieChartType: string = 'pie';
-  public chartPieClicked(e: any): void {
-  }
-  public chartPieHovered(e: any): void {
-  }
-  public pieChartOptions: any = {
-    responsive: true,
-    legend: {
-      display: false,
-      position: "right"
-    }
-  };
-  public pieChartColors: {}[] = [{
-    backgroundColor: [
-      '#5d8aa8',
-      '#e32636',
-      '#efdecd',
-      '#e52b50',
-      '#ffbf00',
-      '#ff033e',
-      '#9966cc',
-      '#a4c639',
-      '#f2f3f4',
-      '#cd9575',
-      '#915c83',
-      '#faebd7',
-      '#008000',
-      '#8db600',
-      '#fbceb1',
-      '#00ffff',
-      '#7fffd4',
-      '#4b5320',
-      '#e9d66b',
-      '#b2beb5',
-      '#87a96b',
-      '#ff9966',
-      '#a52a2a',
-      '#fdee00',
-      '#6e7f80',
-      '#ff2052',
-      '#007fff',
-      '#f0ffff',
-      '#89cff0',
-      '#a1caf1',
-      '#f4c2c2',
-      '#21abcd',
-      '#fae7b5',
-      '#ffe135',
-      '#848482',
-      '#98777b',
-      '#bcd4e6',
-      '#9f8170',
-      '#f5f5dc',
-      '#ffe4c4',
-      '#3d2b1f',
-      '#fe6f5e',
-      '#000000',
-      '#ffebcd',
-      '#318ce7',
-      '#ace5ee',
-      '#faf0be',
-      '#0000ff',
-      '#a2a2d0',
-      '#6699cc',
-      '#0d98ba',
-      '#8a2be2',
-      '#de5d83',
-      '#79443b',
-      '#0095b6',
-      '#e3dac9',
-      '#cc0000',
-      '#006a4e',
-      '#873260',
-      '#0070ff',
-      '#b5a642',
-      '#cb4154',
-      '#1dacd6',
-      '#66ff00',
-      '#bf94e4',
-      '#c32148',
-      '#ff007f',
-      '#08e8de',
-      '#d19fe8',
-      '#f4bbff',
-      '#ff55a3',
-      '#fb607f',
-      '#004225',
-      '#cd7f32',
-      '#a52a2a',
-      '#ffc1cc',
-      '#e7feff',
-      '#f0dc82',
-      '#480607',
-      '#800020',
-      '#deb887',
-      '#cc5500',
-      '#e97451',
-      '#8a3324',
-      '#bd33a4',
-      '#702963',
-      '#007aa5',
-      '#e03c31'
-    ]
-  }];
-
-  /* FORMS */
-  public onDateRangeFormSubmit(formDateRange: FormDateRange): void {
-    this.getDataInternal(formDateRange.startDate, formDateRange.endDate);
-  }
-
-  public onDateRangeQuickButtonClick(days: number): void {
-    let range = this.dateRangeUtils.getIntlFormattedRangeForPastDays(days);
-    this.getDataInternal(range['start'], range['end']);
-  }
 }
