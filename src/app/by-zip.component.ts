36fde36290e621148586f8724d9b21eb10f97430
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-08-27 17:05:24 -0400

    added title functionality to By Zip page

--- src/app/by-zip.component.ts
+++ src/app/by-zip.component.ts
@@ -1,3 +1,4 @@
+import { Title } from '@angular/platform-browser';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from "rxjs/Observable";
@@ -26,13 +27,14 @@ export class ByZipComponent {
     public barChartData: Array<any> = [{ data: [] }];
     public barChartLabels: string[] = [];
 
-    constructor(private http: Http, private route: ActivatedRoute, private dateRangeUtils: DateRangeUtils) {
+    constructor(private http: Http, private route: ActivatedRoute, private dateRangeUtils: DateRangeUtils, private titleService: Title) {
 
     }
 
     ngOnInit() {
         this.route.data
             .subscribe((data: { envSpecific: EnvSpecific }) => {
+                this.titleService.setTitle(data.envSpecific.title + ' | By Zip');
                 this.apiURL = data.envSpecific.apiURL;
             });
 
