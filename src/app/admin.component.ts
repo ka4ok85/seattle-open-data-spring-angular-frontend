eb2b9fb0507979fd308dacaf063df97ee551c29a
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-09-27 22:26:26 -0400

    added admin component

---
+++ src/app/admin.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnDestroy } from '@angular/core';
+
+@Component({
+    selector: '',
+    templateUrl: './templates/admin.html',
+    styles: ['']
+})
+
+export class AdminComponent {
+
+
+}
