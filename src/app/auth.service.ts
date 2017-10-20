4023f82a4918c20ef30297fa866d2c51503bd1ba
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-10-20 14:23:32 -0400

    made Auth backend endpoint dynamic

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/do';
@@ -8,37 +7,31 @@ import 'rxjs/add/operator/map';
 import { Http } from '@angular/http';
 import { AuthenticationResponse } from './core/models/authentication-response';
 
-
-
 @Injectable()
 export class AuthService {
+    isLoggedIn = false;
 
-    constructor(private http: Http) {
+    // store the URL so we can redirect after logging in
+    redirectUrl: string;
 
-    }
+    constructor(private http: Http) { }
 
-
-
-    login(username: String, password: String) {
-
-        console.log("AuthService start login");
-
-
+    login(username: String, password: String, apiURL: string) {
         return this.http.post(
-            'http://localhost:8001/auth/login',
+            //this.apiURL + '/auth/login',
+            apiURL,
             JSON.stringify({
                 username: username,
                 password: password
             }))
             .map(res => res.json())
             .map((res: AuthenticationResponse) => {
-                console.log("AuthService in map login");
-                console.log(res);
+                this.isLoggedIn = true;
                 return Observable.of('token');
             });
     }
 
     logout(): void {
-        ..this.isLoggedIn = false;
+        this.isLoggedIn = false;
     }
 }
