8421554ae59d4322f11010dbca5057c7a8e547b4
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-10-19 20:55:12 -0400

    made login method from AuthService relying on backend

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -6,62 +6,39 @@ import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/delay';
 import 'rxjs/add/operator/map';
 import { Http } from '@angular/http';
+import { AuthenticationResponse } from './core/models/authentication-response';
+
+
 
 @Injectable()
 export class AuthService {
-    isLoggedIn = false;
-
-    // store the URL so we can redirect after logging in
-    redirectUrl: string;
-
 
     constructor(private http: Http) {
 
     }
 
-/*
-    login(): Observable<boolean> {
-        return Observable.of(true).delay(1000).do(val => this.isLoggedIn = true);
-    }
-*/
 
 
-    login() {
-        return this.http.post('http://localhost:8001/auth/login', '{username:"user1", password:"letmein"}')
+    login(username: String, password: String) {
+
+        console.log("AuthService start login");
+
+
+        return this.http.post(
+            'http://localhost:8001/auth/login',
+            JSON.stringify({
+                username: username,
+                password: password
+            }))
             .map(res => res.json())
-            /*
-            .subscribe(
-                // We're assuming the response will be an object
-                // with the JWT on an id_token key
-                data => {
-                    console.log("ok"); 
-                    console.log(data); 
-                    this.isLoggedIn = true
-                },
-                error => {
-                    console.log("bad"); 
-                    console.log(error); 
-                    this.isLoggedIn = false
-                }
-            );
-
-            console.log(this.isLoggedIn);
-
-        return this.isLoggedIn;
-*/
-        /*
-        this.http.post('http://localhost:8001/auth/login', '{username:"user1", password:"letmein"}')
-          .map(res => res.json())
-          .subscribe(
-            // We're assuming the response will be an object
-            // with the JWT on an id_token key
-            data => console.log(data), //localStorage.setItem('id_token', data.id_token),
-            error => console.log(error)
-          );
-*/
-        }
+            .map((res: AuthenticationResponse) => {
+                console.log("AuthService in map login");
+                console.log(res);
+                return Observable.of('token');
+            });
+    }
 
     logout(): void {
-        this.isLoggedIn = false;
+        ..this.isLoggedIn = false;
     }
 }
