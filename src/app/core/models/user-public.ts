fd53835c42667aaa5433cbe69c8048df7fb9a574
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-11-05 10:43:58 -0500

    added model to current user profile

---
+++ src/app/core/models/user-public.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+export class UserPublic {
+    public admin: boolean;
+    public createdAt: number;
+    public email: string;
+    public login: string;
+    public role: string;
+    public status: string;
+}
